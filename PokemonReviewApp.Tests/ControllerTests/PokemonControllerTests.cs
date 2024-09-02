595d5f277c38b7fcefd224220ef4d6b32178f185
Dom Hall <domjshall@gmail.com>
2024-09-02 19:38:13 +0100

    api controller test

---
+++ PokemonReviewApp.Tests/ControllerTests/PokemonControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using AutoMapper.Configuration.Annotations;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Controllers;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Tests.ControllerTests
+{
+    public class PokemonControllerTests
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+        private readonly IReviewRepository _reviewRepository;
+        private readonly IMapper _mapper;
+        public PokemonControllerTests()
+        {
+            _pokemonRepository = A.Fake<IPokemonRepository>();
+            _reviewRepository = A.Fake<IReviewRepository>();
+            _mapper = A.Fake<IMapper>();
+        }
+
+        [Fact]
+        public void PokemonController_GetPokemons_ReturnOK()
+        {
+            var pokemons = A.Fake<ICollection<PokemonDto>>();
+            var pokemonList = A.Fake<List<PokemonDto>>();
+            A.CallTo(() => _mapper.Map<List<PokemonDto>>(pokemons)).Returns(pokemonList);
+            var controller = new PokemonController(_pokemonRepository, _reviewRepository, _mapper);
+
+            var result = controller.GetPokemons();
+
+            result.Should().BeOfType(typeof(OkObjectResult));
+        }
+    }
+}
